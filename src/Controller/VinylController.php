042227b0153c29d9ed79aba0fa61429ab5744022
AndrewS <andreyspace12@gmail.com>
2023-07-10 23:07:42 +0300

    Setting createdAt

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -9,6 +9,9 @@ use function Symfony\Component\String\u;
 
 class VinylController extends AbstractController
 {
+    /**
+     * @return Response
+     */
     #[Route('/', name: 'app_homepage', methods: ['GET'])]
     public function homepage(): Response
     {
@@ -30,13 +33,42 @@ class VinylController extends AbstractController
      * @param string|null $slug
      * @return Response
      */
-    #[Route('/browse/{slug}', name: 'app_browse', methods: ['GET'])]
+    #[Route('/browse/{slug}', name: 'app_browse')]
     public function browse(string $slug = null): Response
     {
         $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
-
+        $mixes = $this->getMixes();
         return $this->render('vinyl/browse.html.twig', [
-            'genre' => $genre
+            'genre' => $genre,
+            'mixes' => $mixes,
         ]);
+    }
+
+    /**
+     * @return array[]
+     */
+    private function getMixes(): array
+    {
+        // temporary fake "mixes" data
+        return [
+            [
+                'title' => 'PB & Jams',
+                'trackCount' => 14,
+                'genre' => 'Rock',
+                'createdAt' => new \DateTime('2021-10-02'),
+            ],
+            [
+                'title' => 'Put a Hex on your Ex',
+                'trackCount' => 8,
+                'genre' => 'Heavy Metal',
+                'createdAt' => new \DateTime('2022-04-28'),
+            ],
+            [
+                'title' => 'Spice Grills - Summer Tunes',
+                'trackCount' => 10,
+                'genre' => 'Pop',
+                'createdAt' => new \DateTime('2019-06-20'),
+            ],
+        ];
     }
 }
