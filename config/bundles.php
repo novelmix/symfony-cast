1d2c37fed3627bd286239308575df592e4e48c6b
AndrewS <andreyspace12@gmail.com>
2023-07-15 18:30:25 +0300

    Added docker and doctrine

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,6 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
